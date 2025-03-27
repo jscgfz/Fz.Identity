3087aeb5ab31e1155156fc81e57b6169e83e8331
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:45:33 -0500

    Core.Domain.Primitives | Add | IAuditableEntity contract

---
+++ Fz.Core.Domain.Primitives/Abstractions/IAuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Fz.Core.Domain.Primitives.Abstractions;
+
+public interface IAuditableEntity<TUser> where TUser : IEquatable<TUser>
+{
+  DateTime CreatedAtUtc { get; }
+  TUser CreatedBy { get; }
+  DateTime? ModifiedAtUtc { get; }
+  TUser? ModifiedBy { get; }
+}
