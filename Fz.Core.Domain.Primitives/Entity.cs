68654f620fdb33087a2f64d3e7654478c3b8802c
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:46:26 -0500

    Core.Domain.Primitives | Add | Entity abstractions

---
+++ Fz.Core.Domain.Primitives/Entity.cs
@@ -0,0 +1,25 @@
+﻿using Fz.Core.Domain.Primitives.Abstractions;
+
+namespace Fz.Core.Domain.Primitives;
+
+public abstract class Entity<TKey, TUser> : IBaseEntity<TKey>, IAuditableEntity<TUser>, ISoftDeleteableEntity<TUser>
+  where TKey : IEquatable<TKey>
+  where TUser : IEquatable<TUser>
+{
+#pragma warning disable CS8618
+  public TKey Id { get; set; }
+#pragma warning restore CS8618
+  public DateTime CreatedAtUtc { get; }
+#pragma warning disable CS8618
+  public TUser CreatedBy { get; }
+#pragma warning restore CS8618
+  public DateTime? ModifiedAtUtc { get; }
+  public TUser? ModifiedBy { get; }
+  public bool IsDeleted { get; }
+  public DateTime? DeletedAtUtc { get; }
+  public TUser? DeletedBy { get; }
+}
+
+public abstract class Entity<TKey> : Entity<TKey, Guid>
+  where TKey : IEquatable<TKey>
+{ }
