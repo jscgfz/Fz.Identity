4fc8ebebb7d1b2f9ef805f7afeaf28345c0a0374
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:43:45 -0500

    Core.Cache | Add | ICancellableCacheManager Contract

---
+++ Fz.Core.Cache/Abstractions/ICancellableCacheManager.cs
@@ -0,0 +1,14 @@
+﻿namespace Fz.Core.Cache.Abstractions;
+
+public interface ICancellableCacheManager
+{
+  Task<string?> TryGetStringAsync(string key, CancellationToken cancellationToken);
+  Task<string> GetOrSetStringAsync(string key, Func<Task<string>> func, CancellationToken cancellationToken);
+  Task<string> GetOrSetStringAsync(string key, Func<Task<string>> func, TimeSpan expiration, CancellationToken cancellationToken);
+  Task SetStringAsync(string key, string value, CancellationToken cancellationToken);
+  Task SetStringAsync(string key, string value, TimeSpan expiration, CancellationToken cancellationToken);
+  Task<TResult?> TryGetAsync<TResult>(string key, CancellationToken cancellationToken);
+  Task<TResult> GetOrSetAsync<TResult>(string key, Func<Task<TResult>> func, CancellationToken cancellationToken);
+  Task<TResult> GetOrSetAsync<TResult>(string key, Func<Task<TResult>> func, TimeSpan expiration, CancellationToken cancellationToken);
+  Task RemoveAsync(string key, CancellationToken cancellationToken);
+}
