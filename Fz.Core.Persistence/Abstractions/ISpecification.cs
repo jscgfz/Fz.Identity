9601271340dfe13ee9ab2de4313caff25c3730c7
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:48:48 -0500

    Core.Persistence | Add | ISpecification contract

---
+++ Fz.Core.Persistence/Abstractions/ISpecification.cs
@@ -0,0 +1,8 @@
+﻿namespace Fz.Core.Persistence.Abstractions;
+
+public interface ISpecification<TQuery, TResult>
+  where TQuery : class
+  where TResult : class
+{
+  IQueryable<TResult> Apply(IQueryable<TQuery> query);
+}
