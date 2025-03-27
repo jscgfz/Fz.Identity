8278efade067667e850435a69850bab7298f2d40
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:46:01 -0500

    Core.Domain.Primitives | Add | ISoftDeleteableEntity contract

---
+++ Fz.Core.Domain.Primitives/Abstractions/ISoftDeleteableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Fz.Core.Domain.Primitives.Abstractions;
+
+public interface ISoftDeleteableEntity<TUser> where TUser : IEquatable<TUser>
+{
+  public bool IsDeleted { get; }
+  public DateTime? DeletedAtUtc { get; }
+  public TUser? DeletedBy { get; }
+}
