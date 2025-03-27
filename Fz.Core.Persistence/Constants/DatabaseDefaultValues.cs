7ddc4afabf9472264397ec8b1ede84c80e0ca799
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:54:36 -0500

    Core.Persistence | Add | DatabaseDefault constants

---
+++ Fz.Core.Persistence/Constants/DatabaseDefaultValues.cs
@@ -0,0 +1,7 @@
+﻿namespace Fz.Core.Persistence.Constants;
+
+internal static class DatabaseDefaultValues
+{
+  public const string UtcNow = "getutcdate()";
+  public const string Guid = "newsequentialid()";
+}
