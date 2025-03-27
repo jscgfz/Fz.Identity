c4910d4b5a596353c80492a4fa4e4a1ce1bd508e
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:55:15 -0500

    Core.Persistence | Add | AuditableEntityConvention

---
+++ Fz.Core.Persistence/Conventions/AuditableEntityConvention.cs
@@ -0,0 +1,30 @@
+﻿using Fz.Core.Domain.Primitives.Abstractions;
+using Fz.Core.Persistence.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Fz.Core.Persistence.Conventions;
+
+public sealed class AuditableEntityConvention : IModelFinalizingConvention
+{
+  public void ProcessModelFinalizing(IConventionModelBuilder modelBuilder, IConventionContext<IConventionModelBuilder> context)
+    => modelBuilder
+      .Metadata
+      .GetEntityTypes()
+      .Where(entityType => entityType.ClrType is { IsAbstract: false, IsInterface: false } && typeof(IAuditableEntity<>).IsAssignableFrom(entityType.ClrType))
+      .ToList()
+      .ForEach(entityType =>
+      {
+        IConventionProperty date = entityType.FindProperty(nameof(IAuditableEntity<Guid>.CreatedAtUtc)) ?? throw new NullReferenceException();
+        IConventionProperty user = entityType.FindProperty(nameof(IAuditableEntity<Guid>.CreatedBy)) ?? throw new NullReferenceException();
+        date.SetDefaultValueSql(DatabaseDefaultValues.UtcNow);
+        object? asignation = user.ClrType switch
+        {
+          Type t when t == typeof(Guid) => user.SetDefaultValue(Guid.Empty),
+          Type t when t == typeof(int) => user.SetDefaultValue(0),
+          _ => throw new NotImplementedException()
+        };
+      });
+}
