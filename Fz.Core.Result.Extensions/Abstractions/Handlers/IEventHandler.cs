b9473e295481debc4ed70fbfafc088680afaa4d4
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 17:00:38 -0500

    Core.Result .Extensions| Add | EventHandler contract

---
+++ Fz.Core.Result.Extensions/Abstractions/Handlers/IEventHandler.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Fz.Core.Result.Extensions.Abstractions.Handlers;
+
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent>
+  where TEvent : IEvent { }
