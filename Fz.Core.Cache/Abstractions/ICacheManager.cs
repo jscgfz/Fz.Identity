0b8e05676862271ed3242c4b9d3fb92b4e4effba
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:43:01 -0500

    Core.Cache | Add | ICacheManager Contract

---
+++ Fz.Core.Cache/Abstractions/ICacheManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Fz.Core.Cache.Abstractions;
+
+public interface ICacheManager : ICancellableCacheManager
+{
+  Task<string?> TryGetStringAsync(string key);
+  Task<string> GetOrSetStringAsync(string key, Func<Task<string>> func);
+  Task<string> GetOrSetStringAsync(string key, Func<Task<string>> func, TimeSpan expiration);
+  Task SetStringAsync(string key, string value);
+  Task SetStringAsync(string key, string value, TimeSpan expiration);
+  Task<TResult?> TryGetAsync<TResult>(string key);
+  Task<TResult> GetOrSetAsync<TResult>(string key, Func<Task<TResult>> func);
+  Task<TResult> GetOrSetAsync<TResult>(string key, Func<Task<TResult>> func, TimeSpan expiration);
+  Task RemoveAsync(string key);
+  IAsyncEnumerable<string> KeysAsync();
+  IAsyncEnumerable<string> KeysAsync([StringSyntax(StringSyntaxAttribute.Regex)] string pattern);
+}
