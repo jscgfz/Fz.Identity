a41b22355e53e3e187fa9d91ef0d2e3fa259c555
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:49:25 -0500

    Core.Persistence | Add | IReadOnlyDbContext contract

---
+++ Fz.Core.Persistence/Abstractions/IReadOnlyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Fz.Core.Persistence.Abstractions;
+
+public interface IReadOnlyDbContext : IDbSpecificationContext
+{
+  IQueryable<TQuery> Repository<TQuery>() where TQuery : class;
+  ValueTask<TQuery?> FindByIdAsync<TQuery>(params object[] keys) where TQuery : class;
+  Task<TQuery?> FirstOrDefaultAsync<TQuery>(Expression<Func<TQuery, bool>> predicate) where TQuery : class;
+  Task<IEnumerable<TQuery>> ListAsync<TQuery>(Expression<Func<TQuery, bool>> predicate) where TQuery : class;
+  Task<int> CountAsync<TQuery>(Expression<Func<TQuery, bool>> predicate) where TQuery : class;
+  Task<bool> AnyAsync<TQuery>(Expression<Func<TQuery, bool>> predicate) where TQuery : class;
+}
