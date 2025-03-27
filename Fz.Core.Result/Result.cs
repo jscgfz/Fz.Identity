52341d13300cc41c0b88d5b78edaf6e673074579
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:59:02 -0500

    Core.Result | Add | Result and ValueResult implementations

---
+++ Fz.Core.Result/Result.cs
@@ -0,0 +1,42 @@
+﻿using Fz.Core.Result.Common;
+
+namespace Fz.Core.Result;
+
+public class Result
+{
+  public ResultType Type { get; private set; }
+  public IEnumerable<Error> Errors { get; private set; }
+  public bool IsSuccess => ResultTypes.IsSuccess(Type);
+  public bool IsFailure => !IsSuccess;
+
+  protected internal Result(ResultType type, IEnumerable<Error>? errors = default)
+  {
+    Guards.ThrowIfNull(type, nameof(type));
+    errors ??= [];
+    Guards.Assert(
+      () => ResultTypes.IsValidType(type),
+      new ArgumentException("Tipo de Resultado invalido", nameof(type)));
+    Guards.Assert(
+      () => !ResultTypes.IsSuccess(type) && (!errors.Any() || errors.Any(e => e == Error.Empty)),
+      new ArgumentException("El resultado debe tener errores especificados", nameof(errors)));
+    Guards.Assert(
+      () => ResultTypes.IsSuccess(type) && (errors.Any(e => e != Error.Empty) || errors.Any()),
+      new ArgumentException("El resultado no debe tener errores especificados", nameof(errors)));
+    (Type, Errors) = (type, errors);
+  }
+
+  public static Result<TValue> From<TValue>(TValue? value, ResultType onErrorType, IEnumerable<Error> onError)
+    => value is null ? Failure<TValue>(onErrorType, onError) : Success(value);
+  public static async Task<Result<TValue>> From<TValue>(Task<TValue?> promise, ResultType onErrorType, IEnumerable<Error> onError)
+    => From(await promise, onErrorType, onError);
+  public static Result Success() => new(ResultTypes.OK, [Error.Empty]);
+  public static Result Success(ResultType type) => new(type, [Error.Empty]);
+  public static Result<TValue> Success<TValue>(TValue value) => new(ResultTypes.OK, value, [Error.Empty]);
+  public static Result<TValue> Success<TValue>(TValue value, ResultType type) => new(type, value, [Error.Empty]);
+  public static Result ValidationError(IEnumerable<Error> errors) => new(ResultTypes.ValidationError, errors);
+  public static Result<TValue> ValidationError<TValue>(IEnumerable<Error> errors) => new(ResultTypes.ValidationError, default, errors);
+  public static Result UnhandledError(IEnumerable<Error> errors) => new(ResultTypes.UnhandledError, errors);
+  public static Result<TValue> UnhandledError<TValue>(IEnumerable<Error> errors) => new(ResultTypes.UnhandledError, default, errors);
+  public static Result Failure(ResultType type, IEnumerable<Error> errors) => new(type, errors);
+  public static Result<TValue> Failure<TValue>(ResultType type, IEnumerable<Error> errors) => new(type, default, errors);
+}
---
+++ Fz.Core.Result/ResultT.cs
@@ -0,0 +1,21 @@
+﻿using Fz.Core.Result.Common;
+
+namespace Fz.Core.Result;
+
+public sealed class Result<TValue>(
+  ResultType type, TValue? value, IEnumerable<Error>? errors = default) : Result(type, errors)
+{
+  private readonly TValue? _value = value;
+  public TValue Value
+  {
+    get
+    {
+      Guards.Assert(() => IsSuccess, new InvalidOperationException("No hay valores para un resultado erroneo"));
+      Guards.ThrowIfNull(_value, nameof(Value));
+      return _value!;
+    }
+  }
+
+  public static implicit operator Result<TValue>(TValue? value)
+    => From(value, ResultTypes.NotFound, [new Error("Object.NotFound", "Elemento no encontrado")]);
+}
