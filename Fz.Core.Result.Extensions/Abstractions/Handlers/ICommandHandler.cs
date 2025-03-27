17a76c2a9846f496b72f75520f7239446706e1f5
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 17:00:13 -0500

    Core.Result .Extensions| Add | CommandHandler contract

---
+++ Fz.Core.Result.Extensions/Abstractions/Handlers/ICommandHandler.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Fz.Core.Result.Extensions.Abstractions.Handlers;
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+  where TCommand : ICommand<TResponse>
+  where TResponse : Result { }
