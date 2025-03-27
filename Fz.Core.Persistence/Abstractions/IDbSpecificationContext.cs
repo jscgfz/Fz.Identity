744fb09b52336aa5199c81e5b8a7926e5d4b3a9e
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:50:01 -0500

    Core.Persistence | Add | IDbSpecificationContext contract

---
+++ Fz.Core.Persistence/Abstractions/IDbSpecificationContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Fz.Core.Persistence.Abstractions;
+
+public interface IDbSpecificationContext
+{
+  Task<TResult?> FirstOrDefaultAsync<TQuery, TResult>(ISpecification<TQuery, TResult> spec)
+    where TQuery : class
+    where TResult : class;
+  Task<IEnumerable<TResult>> ListAsync<TQuery, TResult>(ISpecification<TQuery, TResult> spec)
+    where TQuery : class
+    where TResult : class;
+  Task<int> CountAsync<TQuery, TResult>(ISpecification<TQuery, TResult> spec)
+    where TQuery : class
+    where TResult : class;
+  Task<bool> AnyAsync<TQuery, TResult>(ISpecification<TQuery, TResult> spec)
+    where TQuery : class
+    where TResult : class;
+}
