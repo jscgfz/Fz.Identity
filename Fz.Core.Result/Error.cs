cf7aa4032c7c7793ffe72f6c774eed656f97acc1
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:57:05 -0500

    Core.Result | Add | Error record

---
+++ Fz.Core.Result/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace Fz.Core.Result;
+
+public sealed record Error
+{
+  public string? Code { get; private set; }
+  public string Description { get; private set; }
+  public static Error Empty => new(string.Empty);
+
+  public Error(string description)
+    => Description = description;
+  public Error(string code, string description)
+    => (Code, Description) = (code, description);
+}
