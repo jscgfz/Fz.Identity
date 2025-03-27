03f564af8581a5484b9e08818f6f4d267c4111a6
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:46:42 -0500

    Core.Domain.Primitives | Add | MasterEntity abstractions

---
+++ Fz.Core.Domain.Primitives/MasterEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Fz.Core.Domain.Primitives;
+
+public abstract class MasterEntity<TKey, TUser> : Entity<TKey, TUser>
+  where TKey : IEquatable<TKey>
+  where TUser : IEquatable<TUser>
+{
+#pragma warning disable CS8618
+  public virtual string Name { get; set; }
+  public virtual string Description { get; set; }
+#pragma warning restore CS8618
+}
+
+public abstract class MasterEntity<TKey> : MasterEntity<TKey, Guid>
+  where TKey : IEquatable<TKey>
+{ }
