437917a599beb8f5754c7d640486d97b7d047c12
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 17:00:24 -0500

    Core.Result .Extensions| Add | QueryHandler contract

---
+++ Fz.Core.Result.Extensions/Abstractions/Handlers/IQueryHandler.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Fz.Core.Result.Extensions.Abstractions.Handlers;
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+  where TQuery : IQuery<TResponse>
+  where TResponse : Result { }
