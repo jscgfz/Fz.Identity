e6ab5c9c5c41df33ecc9b40f6688c4c44fe43f7a
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:54:06 -0500

    Core.Persistence | Add | SoftDeleteableEntityConvention

---
+++ Fz.Core.Persistence/Conventions/SoftDeleteableEntityConvention.cs
@@ -0,0 +1,27 @@
+﻿using Fz.Core.Domain.Primitives.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.Linq.Expressions;
+
+namespace Fz.Core.Persistence.Conventions;
+
+public sealed class SoftDeleteableEntityConvention : IModelFinalizingConvention
+{
+  public void ProcessModelFinalizing(IConventionModelBuilder modelBuilder, IConventionContext<IConventionModelBuilder> context)
+    => modelBuilder
+      .Metadata
+      .GetEntityTypes()
+      .Where(entityType => entityType.ClrType is { IsAbstract: false, IsInterface: false } && typeof(ISoftDeleteableEntity<>).IsAssignableFrom(entityType.ClrType))
+      .ToList()
+      .ForEach(entityType =>
+      {
+        ParameterExpression param = Expression.Parameter(entityType.ClrType, "row");
+        MemberExpression prop = Expression.PropertyOrField(param, nameof(ISoftDeleteableEntity<Guid>.IsDeleted));
+        LambdaExpression exp = Expression.Lambda(Expression.Equal(prop, Expression.Constant(false)), param);
+        entityType.SetQueryFilter(exp);
+        IConventionProperty state = entityType.FindProperty(nameof(ISoftDeleteableEntity<Guid>.IsDeleted)) ?? throw new NullReferenceException();
+        state.SetDefaultValue(false);
+      });
+}
