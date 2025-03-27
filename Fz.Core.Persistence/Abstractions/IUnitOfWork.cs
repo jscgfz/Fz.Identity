c80a528dc59c12036c5a5fec9a2d15a6254e4c77
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:51:13 -0500

    Core.Persistence | Add | IUnitOfWork contract

---
+++ Fz.Core.Persistence/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Fz.Core.Persistence.Abstractions;
+
+public interface IUnitOfWork
+{
+  int SaveChanges();
+  int SaveChanges(bool acceptAllChangesOnSuccess);
+  Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+  Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+  Task<IDbContextTransaction> BeginTransaction(CancellationToken cancellationToken = default);
+}
