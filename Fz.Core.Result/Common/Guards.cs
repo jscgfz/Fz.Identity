8dcf7d1728eac1c1b9e5bebc8f081151e7fa3a51
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:56:19 -0500

    Core.Result | Add | Guards methods

---
+++ Fz.Core.Result/Common/Guards.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace Fz.Core.Result.Common;
+
+public sealed class Guards
+{
+  public static void Assert(Expression<Func<bool>> predicate, Exception ex)
+  {
+    ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
+    if(!predicate.Compile().Invoke()) throw ex;
+  }
+
+  public static void ThrowIfNull(object? argument, string? argumentName = null)
+    => ArgumentNullException.ThrowIfNull(argument, argumentName);
+
+  public static void ThrowIfNullOrEmpty(string? argument, string? argumentName = null)
+    => ArgumentNullException.ThrowIfNullOrEmpty(argument, argumentName);
+
+  public static void ThrowIfNullOrWhiteSpace(string? argument, string? argumentName = null)
+    => ArgumentNullException.ThrowIfNullOrWhiteSpace(argument, argumentName);
+}
