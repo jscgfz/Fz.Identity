f022e8f97bb5a09d32a35b421b6b5a2db71d044a
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-03-27 16:50:14 -0500

    Core.Persistence | Add | IDbContext contract

---
+++ Fz.Core.Persistence/Abstractions/IDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Fz.Core.Persistence.Abstractions;
+
+public interface IDbContext : IReadOnlyDbContext
+{
+  void Add<TQuery>(TQuery entity) where TQuery : class;
+  void AddRange<TQuery>(IEnumerable<TQuery> entities) where TQuery : class;
+  void Update<TQuery>(TQuery entity) where TQuery : class;
+  void UpdateRange<TQuery>(IEnumerable<TQuery> entities) where TQuery : class;
+  void Delete<TQuery>(TQuery entity) where TQuery : class;
+  void DeleteRange<TQuery>(IEnumerable<TQuery> entities) where TQuery : class;
+}
