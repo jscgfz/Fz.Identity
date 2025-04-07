a703479ab11ac31aab1fdfed4b9adce401695617
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-07 17:26:59 -0500

    Core.Domain.Primitives | Add | PaginationSet records

---
+++ Fz.Core.Domain.Primitives/Common/PaginationSet.cs
@@ -0,0 +1,13 @@
+﻿using Fz.Core.Domain.Primitives.Abstractions.Common;
+
+namespace Fz.Core.Domain.Primitives.Common;
+
+public sealed class PaginationSet<TResult>(IEnumerable<TResult> data, int pageCount, int pageIndex, int pageSize) : IPaginatedResult<TResult>
+  where TResult : class
+{
+  public int Count => Data.Count();
+  public IEnumerable<TResult> Data { get; private set; } = data;
+  public int PageCount { get; private set; } = pageCount;
+  public int PageIndex { get; private set; } = pageIndex;
+  public int PageSize { get; private set; } = pageSize;
+}
