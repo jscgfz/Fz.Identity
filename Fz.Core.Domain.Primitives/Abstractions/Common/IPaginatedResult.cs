0e39b80c67f2a0c8ff5a635e205c2aed8fc43a6f
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-07 17:24:56 -0500

    Core.Domain.Primitives | Add | Paginated results contract

---
+++ Fz.Core.Domain.Primitives/Abstractions/Common/IPaginatedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Fz.Core.Domain.Primitives.Abstractions.Common;
+
+public interface IPaginatedResult<TResult>
+  where TResult : class
+{
+  int Count { get; }
+  IEnumerable<TResult> Data { get; }
+  int PageCount { get; }
+  int PageIndex { get; }
+  int PageSize { get; }
+}
