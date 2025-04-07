92645f826f8fb60fe95b247983b1db246c77771d
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-07 17:23:20 -0500

    Core.Cache | Add | DependencyInjection

---
+++ Fz.Core.Cache.InMemory/Extensions/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Fz.Core.Cache.Abstractions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fz.Core.Cache.InMemory.Extensions;
+
+public static class DependencyInjection
+{
+  public static WebApplicationBuilder WithInMemoryCache(this WebApplicationBuilder builder)
+  {
+    builder
+      .Services
+      .AddMemoryCache();
+
+    builder
+      .Services
+      .AddScoped<ICacheManager, InMemoryCache>();
+    return builder;
+  }
+
+  public static WebApplicationBuilder WithKeyedInMemoryCache(this WebApplicationBuilder builder, object serviceKey)
+  {
+    builder
+      .Services
+      .AddMemoryCache();
+
+    builder
+      .Services
+      .AddKeyedScoped<ICacheManager, InMemoryCache>(serviceKey);
+    return builder;
+  }
+}
