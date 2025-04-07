3b0c3901467b300d104f79863df8b7dda3a8047f
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-07 17:26:04 -0500

    Core.Domain.Primitives | Add | Paginationparams abstractions

---
+++ Fz.Core.Domain.Primitives/Common/PaginationParams.cs
@@ -0,0 +1,5 @@
+﻿using Fz.Core.Domain.Primitives.Abstractions.Common;
+
+namespace Fz.Core.Domain.Primitives.Common;
+
+public abstract record PaginationParams(int? PageIndex, int? PageSize, int? Page) : IPaginationParams;
