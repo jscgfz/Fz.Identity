e6913c64cd18a2b83993107675a60deb669e5ca2
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-07 17:27:36 -0500

    Core,Domain.Primitives | Fix | AuditProps set behaviors

--- Fz.Core.Domain.Primitives/Entity.cs
+++ Fz.Core.Domain.Primitives/Entity.cs
@@ -9,15 +9,15 @@ public abstract class Entity<TKey, TUser> : IBaseEntity<TKey>, IAuditableEntity<
 #pragma warning disable CS8618
   public TKey Id { get; set; }
 #pragma warning restore CS8618
-  public DateTime CreatedAtUtc { get; }
+  public DateTime CreatedAtUtc { get; set; }
 #pragma warning disable CS8618
-  public TUser CreatedBy { get; }
+  public TUser CreatedBy { get; set; }
 #pragma warning restore CS8618
-  public DateTime? ModifiedAtUtc { get; }
-  public TUser? ModifiedBy { get; }
-  public bool IsDeleted { get; }
-  public DateTime? DeletedAtUtc { get; }
-  public TUser? DeletedBy { get; }
+  public DateTime? ModifiedAtUtc { get; set; }
+  public TUser? ModifiedBy { get; set; }
+  public bool IsDeleted { get; set; }
+  public DateTime? DeletedAtUtc { get; set; }
+  public TUser? DeletedBy { get; set; }
 }
 
 public abstract class Entity<TKey> : Entity<TKey, Guid>
