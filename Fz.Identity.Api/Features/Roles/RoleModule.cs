5247bbf360967e9007568595e2493614456a6be2
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:41:10 -0500

    Identity.Api | Add | RoleModule Enpoints

---
+++ Fz.Identity.Api/Features/Roles/RoleModule.cs
@@ -0,0 +1,23 @@
+﻿using Fz.Core.Result.Extensions;
+using Fz.Identity.Api.Abstractions;
+using Fz.Identity.Api.Features.Roles.Dtos;
+using Fz.Identity.Api.Features.Roles.Queries.Roles;
+using MediatR;
+
+namespace Fz.Identity.Api.Features.Roles;
+
+public sealed class RoleModule : IIdentityModule
+{
+  public void MapEndpoints(IEndpointRouteBuilder builder)
+  {
+    RouteGroupBuilder group = builder
+      .MapGroup("/roles")
+      .WithTags("roles")
+      .MapToApiVersion(1);
+
+    group.MapGet(string.Empty, async ([AsParameters] RolesQuery query, ISender sender) => await sender.Send(query).ToResult())
+      .RequireAuthorization()
+      .Produces<IEnumerable<RoleDto>>()
+      .WithDescription("Obtiene los roles disponibles en el sistema");
+  }
+}
