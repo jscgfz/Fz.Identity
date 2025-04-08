c8c672118e7bff6a538accbcce1dc8f3115cc869
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:38:01 -0500

    Identity.Api | Add | ApplicationsQuery record

---
+++ Fz.Identity.Api/Features/Masters/Queries/Applications/ApplicationsQuery.cs
@@ -0,0 +1,7 @@
+﻿using Fz.Core.Result;
+using Fz.Core.Result.Extensions.Abstractions;
+using Fz.Identity.Api.Features.Masters.Dtos;
+
+namespace Fz.Identity.Api.Features.Masters.Queries.Applications;
+
+public sealed record ApplicationsQuery() : IQuery<Result<IEnumerable<ApplicationDto>>>;
