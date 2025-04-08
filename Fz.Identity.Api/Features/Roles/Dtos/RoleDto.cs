b4afac14437878687d57301c70c2abde8e250750
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:40:18 -0500

    Identity.Api | Add | Role Dtos

---
+++ Fz.Identity.Api/Features/Roles/Dtos/RoleDto.cs
@@ -0,0 +1,5 @@
+﻿using Fz.Identity.Api.Features.Masters.Dtos;
+
+namespace Fz.Identity.Api.Features.Roles.Dtos;
+
+public sealed record RoleDto(Guid Id, string Name, int? ApplicationId) : MasterDto<Guid>(Id, Name);
