aa54fd74d34920388f6e61c292605a0f62afaaee
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:30:16 -0500

    Identity.Api | Add | ITokenProviderService contract

---
+++ Fz.Identity.Api/Abstractions/Identity/ITokenProviderService.cs
@@ -0,0 +1,11 @@
+﻿using Fz.Core.Result;
+using Fz.Identity.Api.Database.Entities;
+using Fz.Identity.Api.Features.Auth.Dtos;
+
+namespace Fz.Identity.Api.Abstractions.Identity;
+
+public interface ITokenProviderService
+{
+  Task<Result<IdentityResponseDto>> GenerateToken(User user, int applicationId);
+  Task<Result<IdentityResponseDto>> RefreshToken();
+}
