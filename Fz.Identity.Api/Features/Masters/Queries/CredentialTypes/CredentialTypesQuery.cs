432e658b43fa0d4413cfb8f7bb767ae34379adb5
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:38:51 -0500

    Identity.Api | Add | CredentialTypesQuery record

---
+++ Fz.Identity.Api/Features/Masters/Queries/CredentialTypes/CredentialTypesQuery.cs
@@ -0,0 +1,7 @@
+﻿using Fz.Core.Result;
+using Fz.Core.Result.Extensions.Abstractions;
+using Fz.Identity.Api.Features.Masters.Dtos;
+
+namespace Fz.Identity.Api.Features.Masters.Queries.CredentialTypes;
+
+public sealed record CredentialTypesQuery() : IQuery<Result<IEnumerable<CredentialTypeDto>>>;
