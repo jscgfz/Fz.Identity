6316cb3715f4cf452d3cbd296df656b66624c08f
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:29:50 -0500

    Identity.Api | Add | ICredentialValidatorService cotract

---
+++ Fz.Identity.Api/Abstractions/Identity/ICredentialValidatorService.cs
@@ -0,0 +1,12 @@
+﻿using Fz.Core.Result;
+using Fz.Identity.Api.Database.Entities;
+using System.Text.Json;
+
+namespace Fz.Identity.Api.Abstractions.Identity;
+
+public interface ICredentialValidatorService
+{
+  Task<Result<User>> ValidateCredentials(JsonElement credentials);
+
+  Task<Result> CreateCredential(Guid userId, string username);
+}
