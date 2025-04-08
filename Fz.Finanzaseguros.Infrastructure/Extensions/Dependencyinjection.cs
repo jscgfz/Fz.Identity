89be1aca5a82b822033bfb40b4871ccd7fda63e8
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 15:37:50 -0500

    Finanzaseguros.Infra | Add | Dependency injection extension methods

---
+++ Fz.Finanzaseguros.Infrastructure/Extensions/Dependencyinjection.cs
@@ -0,0 +1,32 @@
+﻿using Fz.Finanzaseguros.Application.Abstractions.Services;
+using Fz.Finanzaseguros.Infrastructure.Configuration.Delegates;
+using Fz.Finanzaseguros.Infrastructure.Configuration.Settings;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Refit;
+
+namespace Fz.Finanzaseguros.Infrastructure.Extensions;
+
+public static class Dependencyinjection
+{
+  public static WebApplicationBuilder WithInfrastructure(this WebApplicationBuilder builder)
+  {
+    builder.Services
+      .AddOptions<MpmServiceSettings>()
+      .BindConfiguration(nameof(MpmServiceSettings))
+      .ValidateDataAnnotations()
+      .ValidateOnStart();
+
+    builder
+      .Services
+      .AddRefitClient<IMpmService>()
+      .ConfigureHttpClient((provider, client) =>
+      {
+        MpmServiceSettings settings = provider.GetRequiredService<IOptions<MpmServiceSettings>>().Value;
+        client.BaseAddress = new(settings.BaseUrl);
+      })
+      .AddHttpMessageHandler<MpmAuthenticationDelegate>();
+    return builder;
+  }
+}
