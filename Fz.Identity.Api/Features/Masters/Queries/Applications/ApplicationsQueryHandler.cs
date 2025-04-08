11bc3c05ad4055d121f4ff6065c53c25499038ba
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:38:15 -0500

    Identity.Api | Add | ApplicationsQueryHandler

---
+++ Fz.Identity.Api/Features/Masters/Queries/Applications/ApplicationsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Fz.Core.Persistence.Abstractions;
+using Fz.Core.Result;
+using Fz.Core.Result.Extensions;
+using Fz.Core.Result.Extensions.Abstractions.Handlers;
+using Fz.Identity.Api.Database.Entities;
+using Fz.Identity.Api.Features.Masters.Dtos;
+using Fz.Identity.Api.Settings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fz.Identity.Api.Features.Masters.Queries.Applications;
+
+public sealed class ApplicationsQueryHandler(IServiceProvider provider) : IQueryHandler<ApplicationsQuery, Result<IEnumerable<ApplicationDto>>>
+{
+  private readonly IReadOnlyDbContext _context
+    = provider.GetRequiredKeyedService<IReadOnlyDbContext>(ContextTypes.Identity);
+
+  public Task<Result<IEnumerable<ApplicationDto>>> Handle(ApplicationsQuery request, CancellationToken cancellationToken)
+#pragma warning disable CS8620 // El argumento no se puede usar para el parámetro debido a las diferencias en la nulabilidad de los tipos de referencia.
+    => Result.From(
+      _context.Repository<Application>().Select(row => new ApplicationDto(row.Id, row.Name)).ToListAsync(cancellationToken),
+      ResultTypes.NotFound,
+      [new Error("Applications.NotFound")]
+    )
+    .Map(result => result.AsEnumerable());
+#pragma warning restore CS8620 // El argumento no se puede usar para el parámetro debido a las diferencias en la nulabilidad de los tipos de referencia.
+}
