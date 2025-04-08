1aced44a9657bbded314a41b31650ec28fbb96f1
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:41:56 -0500

    Identity.Api | Add User Dtos

---
+++ Fz.Identity.Api/Features/Users/Dtos/UserAddedResponseDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Fz.Identity.Api.Features.Users.Dtos;
+
+public sealed record UserAddedResponseDto(
+  Guid Id,
+  string UserName
+);
---
+++ Fz.Identity.Api/Features/Users/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using Fz.Identity.Api.Features.Roles.Dtos;
+
+namespace Fz.Identity.Api.Features.Users.Dtos;
+
+public sealed record UserDto(
+  Guid Id,
+  string Name,
+  string Surname,
+  string IdentificationNumber,
+  string Email,
+  IEnumerable<RoleDto>? Roles
+);
