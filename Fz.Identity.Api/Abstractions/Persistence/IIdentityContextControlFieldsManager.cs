f8c2bd593cfc4606b125789bd78ae7a580539358
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 08:32:11 -0500

    Identity.Api | Add | IIdentityContextControlFieldsManager contract

---
+++ Fz.Identity.Api/Abstractions/Persistence/IIdentityContextControlFieldsManager.cs
@@ -0,0 +1,9 @@
+﻿using Fz.Core.Persistence.Abstractions;
+
+namespace Fz.Identity.Api.Abstractions.Persistence;
+
+public interface IIdentityContextControlFieldsManager : IContextControlFieldsManager<Guid>
+{
+  int? ApplicationId { get; }
+  IEnumerable<Guid> RoleIds { get; }
+}
