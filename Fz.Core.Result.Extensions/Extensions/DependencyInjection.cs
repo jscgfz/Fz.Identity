fe0709612002439a4f42d5bedcd16d0b4a59b7d8
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 08:25:51 -0500

    Core.Result.Extensions | Add | DependencyInjection

---
+++ Fz.Core.Result.Extensions/Extensions/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fz.Core.Result.Extensions.Extensions;
+
+public static class DependencyInjection
+{
+  public static WebApplicationBuilder WithResultExtensions(this WebApplicationBuilder builder, Action<MediatRServiceConfiguration>? options = default)
+  {
+    builder.Services.AddMediatR(mediatr => options?.Invoke(mediatr));
+    return builder;
+  }
+}
