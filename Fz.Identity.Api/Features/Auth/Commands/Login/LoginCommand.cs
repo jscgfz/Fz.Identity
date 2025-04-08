94273ee83c21f4af53d3931b38135a02e10e817d
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:32:29 -0500

    Identity.Api | Add | LoginCommand record

---
+++ Fz.Identity.Api/Features/Auth/Commands/Login/LoginCommand.cs
@@ -0,0 +1,8 @@
+﻿using Fz.Core.Result;
+using Fz.Core.Result.Extensions.Abstractions;
+using Fz.Identity.Api.Features.Auth.Dtos;
+using System.Text.Json;
+
+namespace Fz.Identity.Api.Features.Auth.Commands.Login;
+
+public record class LoginCommand(int ApplicationId, int CredentialType, JsonElement Credentials) : ICommand<Result<IdentityResponseDto>>;
