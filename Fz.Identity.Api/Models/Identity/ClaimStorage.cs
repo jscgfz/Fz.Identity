0126d3b14c01d065e2d8c5ede5b5d1652998c932
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 10:30:13 -0500

    Identity.Api | Add | Identity Models

---
+++ Fz.Identity.Api/Models/Identity/ClaimStorage.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace Fz.Identity.Api.Models.Identity;
+
+public sealed record ClaimStorage
+{
+  public string Type { get; init; }
+  public string Value { get; init; }
+
+  public ClaimStorage(string type, string value) => (Type, Value) = (type, value);
+
+  public static ClaimStorage FromClaim(Claim claim) => new(claim.Type, claim.Value);
+  public Claim ToClaim() => new(Type, Value);
+}
---
+++ Fz.Identity.Api/Models/Identity/IdentityStorage.cs
@@ -0,0 +1,5 @@
+﻿using Fz.Identity.Api.Database.Entities;
+
+namespace Fz.Identity.Api.Models.Identity;
+
+public sealed record IdentityStorage(IEnumerable<ClaimStorage> Claims, Application App, User User, IEnumerable<Role> Roles);
