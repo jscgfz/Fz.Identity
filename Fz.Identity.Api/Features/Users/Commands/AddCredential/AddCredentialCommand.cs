7e99927e343254a6937aa39cdb0e9d0692f1e216
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:42:52 -0500

    Identity.Api | Add | AddCredentialCommand record

---
+++ Fz.Identity.Api/Features/Users/Commands/AddCredential/AddCredentialCommand.cs
@@ -0,0 +1,7 @@
+﻿using Fz.Core.Result;
+using Fz.Core.Result.Extensions.Abstractions;
+using Fz.Identity.Api.Settings;
+
+namespace Fz.Identity.Api.Features.Users.Commands.AddCredential;
+
+public sealed record AddCredentialCommand(Guid UserId, CredentialTypes Type, string? Username) : ICommand<Result>;
