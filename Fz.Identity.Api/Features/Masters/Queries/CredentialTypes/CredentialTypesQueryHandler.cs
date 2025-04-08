c8c15bb6182dd6b37054ac018cfd1c0e7bcdc52f
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:39:10 -0500

    Identity.Api | Add | CredentialTypesQueryHandler

---
+++ Fz.Identity.Api/Features/Masters/Queries/CredentialTypes/CredentialTypesQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Fz.Core.Persistence.Abstractions;
+using Fz.Core.Result;
+using Fz.Core.Result.Extensions;
+using Fz.Core.Result.Extensions.Abstractions.Handlers;
+using Fz.Identity.Api.Database.Entities;
+using Fz.Identity.Api.Features.Masters.Dtos;
+using Fz.Identity.Api.Settings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fz.Identity.Api.Features.Masters.Queries.CredentialTypes;
+
+public sealed class CredentialTypesQueryHandler(IServiceProvider provider) : IQueryHandler<CredentialTypesQuery, Result<IEnumerable<CredentialTypeDto>>>
+{
+  private readonly IReadOnlyDbContext _context = provider.GetRequiredKeyedService<IReadOnlyDbContext>(ContextTypes.Identity);
+
+  public Task<Result<IEnumerable<CredentialTypeDto>>> Handle(CredentialTypesQuery request, CancellationToken cancellationToken)
+#pragma warning disable CS8620 // El argumento no se puede usar para el parámetro debido a las diferencias en la nulabilidad de los tipos de referencia.
+    => Result.From(
+        _context.Repository<CredentialType>().Select(row => new CredentialTypeDto(row.Id, row.Name, row.Description)).ToListAsync(cancellationToken),
+        ResultTypes.NotFound,
+        [new Error("credential-types not found", "no se encontraron tipos de credenciales")]
+      )
+    .Map(v => v.AsEnumerable());
+#pragma warning restore CS8620 // El argumento no se puede usar para el parámetro debido a las diferencias en la nulabilidad de los tipos de referencia.
+}
