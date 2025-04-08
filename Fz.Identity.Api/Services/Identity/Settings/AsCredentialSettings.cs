ab94a53f420ddc8120ec9c04836b701143a81bc8
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 10:32:52 -0500

    Identity.Api | Add | Settings cluases

---
+++ Fz.Identity.Api/Services/Identity/Settings/AsCredentialSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Fz.Identity.Api.Services.Identity.Settings;
+
+public sealed class AsCredentialSettings
+{
+  public string BaseUrl { get; set; }
+  public string Signature { get; set; }
+
+  public AsCredentialSettings(string baseUrl, string signature)
+  {
+    BaseUrl = baseUrl;
+    Signature = signature;
+  }
+
+#pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de agregar el modificador "required" o declararlo como un valor que acepta valores NULL.
+  public AsCredentialSettings()
+#pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de agregar el modificador "required" o declararlo como un valor que acepta valores NULL.
+  {
+  }
+}
---
+++ Fz.Identity.Api/Services/Identity/Settings/FzCredentialSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace Fz.Identity.Api.Services.Identity.Settings;
+
+public sealed class FzCredentialSettings
+{
+  public string BaseUrl { get; set; }
+  public int AppId { get; set; }
+  public string Signature { get; set; }
+
+  public FzCredentialSettings(string baseUrl, int appId, string signature)
+  {
+    BaseUrl = baseUrl;
+    AppId = appId;
+    Signature = signature;
+  }
+
+#pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de agregar el modificador "required" o declararlo como un valor que acepta valores NULL.
+  public FzCredentialSettings()
+#pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de agregar el modificador "required" o declararlo como un valor que acepta valores NULL.
+  {
+  }
+}
