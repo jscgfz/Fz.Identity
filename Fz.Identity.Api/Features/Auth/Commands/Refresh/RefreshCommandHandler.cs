942e968ce6979322d9045ad35327036629626ee1
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:33:55 -0500

    Identity.Api | Add | RefreshCommandHandler

---
+++ Fz.Identity.Api/Features/Auth/Commands/Refresh/RefreshCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Fz.Core.Result;
+using Fz.Core.Result.Extensions.Abstractions.Handlers;
+using Fz.Identity.Api.Abstractions.Identity;
+using Fz.Identity.Api.Features.Auth.Dtos;
+using Fz.Identity.Api.Settings;
+
+namespace Fz.Identity.Api.Features.Auth.Commands.Refresh;
+
+public sealed class RefreshCommandHandler(IServiceProvider provider) : ICommandHandler<RefreshCommand, Result<IdentityResponseDto>>
+{
+  private readonly ITokenProviderService _tokenProviderService = provider.GetRequiredKeyedService<ITokenProviderService>(ContextTypes.Identity);
+  public Task<Result<IdentityResponseDto>> Handle(RefreshCommand request, CancellationToken cancellationToken)
+   => _tokenProviderService.RefreshToken();
+}
