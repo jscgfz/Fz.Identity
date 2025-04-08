958b5c06bb7c188eaf7cab0f75a5f7fea7718162
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 10:42:27 -0500

    Identity.Api | Add | project progam main method

---
+++ Fz.Identity.Api/Program.cs
@@ -0,0 +1,45 @@
+using Asp.Versioning.Builder;
+using Fz.Core.Cache.InMemory.Extensions;
+using Fz.Core.Http.Extensions;
+using Fz.Core.Result.Extensions.Extensions;
+using Fz.Identity.Api.Abstractions;
+using Fz.Identity.Api.Extensions;
+using System.Reflection;
+
+var app = WebApplication
+  .CreateBuilder(args)
+  .WithIdentityStore()
+  .WithProblemDetails()
+  .WithEndpointModulesFromAssembly<IIdentityModule>(Assembly.GetExecutingAssembly())
+  .WithInMemoryCache()
+  .WithIdentityOpenApi()
+  .WithJsonWebToken()
+  .WithResultExtensions(options =>
+  {
+    options.RegisterServicesFromAssemblies([Assembly.GetExecutingAssembly()]);
+  })
+  .WithApiVersioning()
+  .WithCors()
+  .WithEndpointsApiExplorer()
+  .Build();
+
+ApiVersionSet set = app.NewApiVersionSet()
+  .HasApiVersion(new(1))
+  .ReportApiVersions()
+  .Build();
+
+app.UseHttpsRedirection();
+app.UseCors();
+app
+  .MapGroup("/api/v{version:apiVersion}")
+  .RequireCors()
+  .WithApiVersionSet(set)
+  .ProducesProblem(StatusCodes.Status500InternalServerError)
+  .ProducesProblem(StatusCodes.Status400BadRequest)
+  .MapEndpointModules<IIdentityModule>();
+
+app.MapOpenApiDocument("finanzauto-identity", servers: app.Configuration.GetSection("ScalarServers").Get<IEnumerable<string>>())
+  .UseAuthentication()
+  .UseAuthorization();
+
+app.Run();
