d64829b398de5ddf1857315965d2ec4318a763e0
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:43:10 -0500

    Identity.Api | Add | AddCredentialCommandHandler

---
+++ Fz.Identity.Api/Features/Users/Commands/AddCredential/AddCredentialCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Fz.Core.Result;
+using Fz.Core.Result.Extensions.Abstractions.Handlers;
+using Fz.Identity.Api.Abstractions.Identity;
+
+namespace Fz.Identity.Api.Features.Users.Commands.AddCredential;
+
+public sealed class AddCredentialCommandHandler(IServiceProvider provider) : ICommandHandler<AddCredentialCommand, Result>
+{
+  private readonly IServiceProvider _provider = provider;
+
+  public Task<Result> Handle(AddCredentialCommand request, CancellationToken cancellationToken)
+    => _provider.GetRequiredKeyedService<ICredentialValidatorService>(request.Type)
+      .CreateCredential(request.UserId, request.Username!);
+}
