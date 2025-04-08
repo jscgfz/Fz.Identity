ecfb3c2620ffdfdf76c9df5b6a910d0d2540d3a5
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:33:29 -0500

    Identity.Api | Add | RefreshCommand record

---
+++ Fz.Identity.Api/Features/Auth/Commands/Refresh/RefreshCommand.cs
@@ -0,0 +1,7 @@
+﻿using Fz.Core.Result;
+using Fz.Core.Result.Extensions.Abstractions;
+using Fz.Identity.Api.Features.Auth.Dtos;
+
+namespace Fz.Identity.Api.Features.Auth.Commands.Refresh;
+
+public sealed record RefreshCommand() : ICommand<Result<IdentityResponseDto>>;
