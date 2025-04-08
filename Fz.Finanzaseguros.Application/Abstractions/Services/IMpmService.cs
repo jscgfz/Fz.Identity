f55bc1d7652730806067cd76604a9c4b6176bc26
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 10:48:00 -0500

    Finanzaseguros.Api | Add | IMpmService contract

---
+++ Fz.Finanzaseguros.Application/Abstractions/Services/IMpmService.cs
@@ -0,0 +1,33 @@
+﻿using Fz.Finanzaseguros.Application.Common.Models.MPM.Request;
+using Fz.Finanzaseguros.Application.Common.Models.MPM.Response;
+using Fz.Finanzaseguros.Application.Common.Models.MPM.Response.Status;
+using Refit;
+
+namespace Fz.Finanzaseguros.Application.Abstractions.Services;
+
+public interface IMpmService
+{
+  [Post("/autenticacion")]
+  Task<MpmTokenResponse> Autenticate([Body] object body, CancellationToken cancellationToken = default);
+
+  [Get("/cliente/{nit}")]
+  Task<MpmClientStatusResponse> GetClient(string nit, CancellationToken cancellationToken = default);
+
+  [Get("/colaborador/{name}")]
+  Task<MpmColaboratorStatusResponse> GetColaborator(string name, CancellationToken cancellationToken = default);
+
+  [Get("/ejecutivos/{nit}")]
+  Task<MpmExecutiveStatusResponse> GetExecutive(string nit, CancellationToken cancellationToken = default);
+
+  [Get("/usuarios/{name}")]
+  Task<MpmUserStatusResponse> GetUser(string name, CancellationToken cancellationToken = default);
+
+  [Get("/compañias/{name}")]
+  Task<MpmCompanyStatusResponse> GetCompany(string name, CancellationToken cancellationToken = default);
+
+  [Patch("/actualizarcliente")]
+  Task<MpmClientUpdatedResponse> UpdateClient([Body] MpmUpdateClientRequest req, CancellationToken cancellationToken = default);
+
+  [Patch("/actualizardireccion")]
+  Task<MpmAddressUpdatedResponse> UpdateAddress([Body] MpmUpdateAddressRequest req, CancellationToken cancellationToken = default);
+}
