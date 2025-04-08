ff695743e609f957a82c15e5e8fa5b43f5e9feb6
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:32:54 -0500

    Identity.Api | Add | LoginCommandHandler

---
+++ Fz.Identity.Api/Features/Auth/Commands/Login/LoginCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using Fz.Core.Result;
+using Fz.Core.Result.Extensions;
+using Fz.Core.Result.Extensions.Abstractions.Handlers;
+using Fz.Identity.Api.Abstractions.Identity;
+using Fz.Identity.Api.Features.Auth.Dtos;
+using Fz.Identity.Api.Settings;
+
+namespace Fz.Identity.Api.Features.Auth.Commands.Login;
+
+public sealed class LoginCommandHandler(IServiceProvider provider) : ICommandHandler<LoginCommand, Result<IdentityResponseDto>>
+{
+  private readonly IServiceProvider _provider = provider;
+  private readonly ITokenProviderService _tokenProvider = provider.GetRequiredKeyedService<ITokenProviderService>(ContextTypes.Identity);
+
+  public Task<Result<IdentityResponseDto>> Handle(LoginCommand request, CancellationToken cancellationToken)
+    => _provider.GetRequiredKeyedService<ICredentialValidatorService>((CredentialTypes)request.CredentialType)
+      .ValidateCredentials(request.Credentials)
+      .Map(result => _tokenProvider.GenerateToken(result, request.ApplicationId));
+}
