dd6dcde61622e31cdd314f4a307c404a5cd43680
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:40:37 -0500

    Identity.Api | Add | RolesQuery record

---
+++ Fz.Identity.Api/Features/Roles/Queries/Roles/RolesQuery.cs
@@ -0,0 +1,7 @@
+﻿using Fz.Core.Result;
+using Fz.Core.Result.Extensions.Abstractions;
+using Fz.Identity.Api.Features.Roles.Dtos;
+
+namespace Fz.Identity.Api.Features.Roles.Queries.Roles;
+
+public sealed record RolesQuery(int? ApplicationId) : IQuery<Result<IEnumerable<RoleDto>>>;
