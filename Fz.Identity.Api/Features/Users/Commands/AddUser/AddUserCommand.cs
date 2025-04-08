eee9beaf4d39227dc5c4bde842725d24205d7bae
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-08 09:43:32 -0500

    Identity.Api | Add | AddUserCommand record

---
+++ Fz.Identity.Api/Features/Users/Commands/AddUser/AddUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using Fz.Core.Result;
+using Fz.Core.Result.Extensions.Abstractions;
+using Fz.Identity.Api.Features.Users.Dtos;
+
+namespace Fz.Identity.Api.Features.Users.Commands.AddUser;
+
+public sealed record AddUserCommand(
+  string Name,
+  string Surname,
+  string UserName,
+  string Email,
+  string IdentificationNumber,
+  string PhoneNamuber
+) : ICommand<Result<UserAddedResponseDto>>;
