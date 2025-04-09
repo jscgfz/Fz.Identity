0620b514290ca0e52696d87cbb11758bba9efe4b
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-09 10:58:10 -0500

    Finanzaseguros.Infra | Add | sarlaft service authentication delegate

---
+++ Fz.Finanzaseguros.Infrastructure/Configuration/Delegates/SarlaftAuthenticationDelegate.cs
@@ -0,0 +1,48 @@
+﻿using Fz.Core.Cache.Abstractions;
+using Fz.Finanzaseguros.Application.Abstractions.Services;
+using Fz.Finanzaseguros.Application.Common.Models.MPM.Response;
+using Fz.Finanzaseguros.Application.Common.Models.Sarlaft.Response;
+using Fz.Finanzaseguros.Infrastructure.Configuration.Settings;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+
+namespace Fz.Finanzaseguros.Infrastructure.Configuration.Delegates;
+
+public sealed class SarlaftAuthenticationDelegate(IServiceProvider provider) : DelegatingHandler
+{
+  private readonly SarlaftServiceSettings _settings
+    = provider.GetRequiredService<IOptions<SarlaftServiceSettings>>().Value;
+  private readonly ICacheManager _cache
+    = provider.GetRequiredService<ICacheManager>();
+
+  protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+  {
+    TimeSpan expiration = TimeSpan.Zero;
+    string token = await _cache.GetOrSetAsync($"{nameof(ISarlaftService)}:token", async () =>
+    {
+      HttpRequestMessage req = new()
+      {
+        Method = HttpMethod.Post,
+        RequestUri = new("/auth/login"),
+        Content = new StringContent(
+          JsonSerializer.Serialize(new { Email = _settings.UserName, _settings.Password }),
+          Encoding.UTF8, "application/json"
+        )
+      };
+
+      HttpResponseMessage resp = await base.SendAsync(req, cancellationToken);
+      resp.EnsureSuccessStatusCode();
+      SarlaftAuthResponse token = await resp.Content.ReadFromJsonAsync<SarlaftAuthResponse>(cancellationToken: cancellationToken) ?? throw new JsonException();
+
+      expiration = (DateTime.UtcNow - token.ExpirationDate);
+
+      return token.AccesToken;
+    }, expiration, cancellationToken);
+
+    request.Headers.Authorization = new("Bearer", token);
+    return await base.SendAsync(request, cancellationToken);
+  }
+}
