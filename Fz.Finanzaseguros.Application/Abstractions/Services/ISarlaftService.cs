3722927d2595d2b15042ba7221fbcd378f803f0c
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-09 10:54:48 -0500

    Finanzaseguros.Application | Add | ISarlaftService contract

---
+++ Fz.Finanzaseguros.Application/Abstractions/Services/ISarlaftService.cs
@@ -0,0 +1,10 @@
+﻿using Fz.Finanzaseguros.Application.Common.Models.Sarlaft.Response;
+using Refit;
+
+namespace Fz.Finanzaseguros.Application.Abstractions.Services;
+
+public interface ISarlaftService
+{
+  [Get("/sarlafts")]
+  Task<SarlaftSarlaftResponse> GetSarlaft([Query] int identificationNumber);
+}
