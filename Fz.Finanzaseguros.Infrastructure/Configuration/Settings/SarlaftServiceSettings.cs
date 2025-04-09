ca0d7a130cc3c42596f5e26bfc3f62ef908512db
Jhon Sebastián Cubillos Gonzalez <167899102+jscgfz@users.noreply.github.com>
2025-04-09 10:56:20 -0500

    Finanzaseguros.Infra | Add | Sarlaft service configuration abstractions

---
+++ Fz.Finanzaseguros.Infrastructure/Configuration/Settings/SarlaftServiceSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Fz.Finanzaseguros.Infrastructure.Configuration.Settings;
+
+public sealed class SarlaftServiceSettings
+{
+  public string BaseUrl { get; set; }
+  public string UserName { get; set; }
+  public string Password { get; set; }
+
+  public SarlaftServiceSettings(string baseUrl, string userName, string password)
+  {
+    BaseUrl = baseUrl;
+    UserName = userName;
+    Password = password;
+  }
+
+#pragma warning disable CS8618
+  public SarlaftServiceSettings() { }
+#pragma warning restore CS8618
+}
