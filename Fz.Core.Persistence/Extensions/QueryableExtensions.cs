ccaaebeea1c61b033b76ff28bf36373147bfe084
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2025-05-13 20:16:13 -0500

    feat(extensions): add Queryable extension to include deleted rows

---
+++ Fz.Core.Persistence/Extensions/QueryableExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fz.Core.Persistence.Extensions;
+public static class QueryableExtensions
+{
+  public static IQueryable<T> IncludeDeleted<T>(this IQueryable<T> query) where T : class
+    => query.IgnoreQueryFilters();
+}
