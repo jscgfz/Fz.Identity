5dd6c45216261857ba07e3c25878dcc9f3f1e196
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2025-07-24 09:08:43 -0500

    feat(infra): add antiforergy and change appsetings
    - Added Antiforergy metadata to use FromForm in endpoints
    - Updates aapssetings to include node id to fauthorization files

---
+++ Fz.Identity.Api/Security/IgnoreAntiforgeryMetadata.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+
+namespace Fz.Identity.Api.Security;
+
+public sealed class IgnoreAntiforgeryMetadata : IAntiforgeryMetadata
+{
+  public static readonly IgnoreAntiforgeryMetadata Instance = new();
+  private IgnoreAntiforgeryMetadata() { }
+
+  public bool RequiresValidation => false;
+}
