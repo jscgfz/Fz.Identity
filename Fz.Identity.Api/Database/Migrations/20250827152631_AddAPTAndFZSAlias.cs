fcc02916ce794fcb682586348ce5ecc9f368b36a
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2025-08-27 10:29:14 -0500

    feat(db): add Atenea PT and FZS alias

---
+++ Fz.Identity.Api/Database/Migrations/20250827152631_AddAPTAndFZSAlias.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fz.Identity.Api.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAPTAndFZSAlias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "conf",
+                table: "Applications",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Alias",
+                value: "AteneaPT");
+
+            migrationBuilder.UpdateData(
+                schema: "conf",
+                table: "Applications",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Alias",
+                value: "Finanzaseguros");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "conf",
+                table: "Applications",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Alias",
+                value: "APT");
+
+            migrationBuilder.UpdateData(
+                schema: "conf",
+                table: "Applications",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Alias",
+                value: null);
+        }
+    }
+}
